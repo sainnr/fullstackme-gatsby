75a60be5bc72f7bfcf21b8339e42de233bdd03b5
Vladimir Salin <salinvs@gmail.com>
2019-07-30 22:43:14 +0100

    brand-new favicon that reflects a refreshed blog appearance.

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,11 +31,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `fullstackme-blog`,
+        short_name: `fullstackme`,
         start_url: `/`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`,
       },
     },
     {
