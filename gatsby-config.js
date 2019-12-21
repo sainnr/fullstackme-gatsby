2ba231cf86974653070d01b33483771888495811
Vladimir Salin <salinvs@gmail.com>
2019-12-21 16:53:32 +0000

    pinterest plugin to pin for interest, lol

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,7 +48,8 @@ module.exports = {
         }
       }
     },
-    `gatsby-plugin-typescript`
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-pinterest`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
